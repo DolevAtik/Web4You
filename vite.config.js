cbe9e5d5c8659a97b88c7124721625f67058aacf
Dolev Atik <Dolev5454@gmail.com>
2026-03-27 22:45:16 +0200

    fix: conditional base path for Vercel and GitHub

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Web4You/',
+  base: process.env.GITHUB_ACTIONS ? '/Web4You/' : '/',
   plugins: [react()],
   publicDir: 'public',
 })
